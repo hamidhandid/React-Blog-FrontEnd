d9261a689d35ab1c5848e40436124c02542a29d7
Hamid <montazeria90@gmail.com>
2020-01-23 17:54:30 +0330

    React Frontend init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        news: [],
+        title: "",
+        text: ""
+    }
+
+    async componentDidMount() {
+       const res = await Axios.get('http://localhost:2992')
+        this.setState({
+            news:res.data
+        })
+    }
+
+    titleAdd = (event) => {
+        this.setState({
+            title: event.target.value
+        })
+    }
+
+    textAdd = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+    reset = () => {
+        this.setState({
+            title: "",
+            text: ""
+        })
+    }
+
+    submit = (event) => {
+        Axios.post('http://localhost:2992', {
+            title: this.state.title,
+            text: this.state.text
+        })
+        .then(res => {
+            this.reset()
+            alert(res.data)
+            Axios.get('http://localhost:2992')
+                .then(res => this.setState({ news: res.data })
+                )
+            })
+    }
+
+    render() {
+        return <div>
+            <input value={this.state.title} onChange={this.titleAdd} />
+            <input value={this.state.text} onChange={this.textAdd} />
+            <button onClick={this.submit}>Add This</button>
+            {this.state.news.map((item, index) =>
+                <div key={index}>
+                    <h1>{item.title}</h1>
+                    <p>{item.body}</p>
+                </div>
+            )}
+        </div>
+    }
+
 }
 
 export default App;
